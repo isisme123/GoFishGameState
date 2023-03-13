791c9da0556de20f9143083655f9d965cdc5dfb8
isisme123 <timogene25@up.edu>
2023-03-13 15:53:45 -0700

    First page done!

--- app/src/main/java/com/example/gamestate/GameState.java
+++ app/src/main/java/com/example/gamestate/GameState.java
@@ -14,10 +14,11 @@ public class GameState {
 
     // Instance variables
     private int currentPlayerIndex; //player who's turn it is
-    private List<Card> deck;
-    private List<Card> playerHands; //Map of the player ID to their hand of cards
+    public List<Card> deck;
+    public List<Card> playerHands; //Map of the player ID to their hand of cards
+    public List<Card> opponentHands;
     //private Map<Integer, Integer> playerScores; // ..scores?!
-    private int yourScore;
+    private int playerScore;
     private int opponentScore;
     private boolean gameOver;
 
@@ -26,13 +27,14 @@ public class GameState {
         currentPlayerIndex = 0;
         deck = createDeck();
         //deck = new ArrayList<>();
-        yourScore = 0;
+        playerScore = 0;
         opponentScore = 0;
         //playerHands = NULL;
 //        playerHands = new HashMap<>();
 //        playerHands.put(0, new ArrayList<Card>());
 //        playerHands.put(1, new ArrayList<Card>());
-        dealCards();
+        dealCards(playerHands);
+        dealCards(opponentHands);
 //        playerScores = new HashMap<>();
 //        playerScores.put(0, 0);
 //        playerScores.put(1, 0);
@@ -42,7 +44,7 @@ public class GameState {
     // Create a new deck of cards
     private List<Card> createDeck() {
         List<Card> deck = new ArrayList<>();
-        for (int suit = 0; suit < 4; suit++) {
+        for (int i = 0; i < 4; i++) {
             for (int rank = 1; rank <= 13; rank++) {
                 deck.add(new Card(rank));
             }
@@ -50,87 +52,101 @@ public class GameState {
         return deck;
     }
 
-    // Deal the cards to the players
-    private void dealCards() {
-        for (int i = 0; i < HAND_SIZE; i++) {
-            Random random = new Random();
-            int k = random.nextInt(deck.size());
-            playerHands.add(deck.get(k));
-            deck.remove(k);
+        // Deal the cards to the players
+        public void dealCards (List < Card > j) {
+            for (int i = 0; i < HAND_SIZE; i++) {
+                Random random = new Random();
+                int k = random.nextInt(deck.size());
+                j.add(deck.get(k));
+                deck.remove(k);
 
-        }
-    }
-
-    // Get the current player index
-    public int getCurrentPlayerIndex() {
-        return currentPlayerIndex;
-    }
-
-    // Set the current player index
-    public void setCurrentPlayerIndex(int currentPlayerIndex) {
-        this.currentPlayerIndex = currentPlayerIndex;
-    }
-
-    // Get the deck
-    public List<Card> getDeck() {
-        return deck;
-    }
-
-    // Set the deck
-    public void setDeck(List<Card> deck) {
-        this.deck = deck;
-    }
-
-    // Get the player hands
-    public Map<Integer, List<Card>> getPlayerHands() {
-        return playerHands;
-    }
-
-    // Set the player hands
-    public void setPlayerHands(Map<Integer, List<Card>> playerHands) {
-        this.playerHands = playerHands;
-    }
-
-    // Get the player scores
-    public Map<Integer, Integer> getPlayerScores() {
-        return playerScores;
-    }
-
-    // Set the player scores
-    public void setPlayerScores(Map<Integer, Integer> playerScores) {
-        this.playerScores = playerScores;
-    }
-
-    // Check if the game is over
-    public boolean isGameOver() {
-        return gameOver;
-    }
-
-    // Set the game over state
-    public void setGameOver(boolean gameOver) {
-        this.gameOver = gameOver;
-    }
-    //This toString print the state of the game.
-    //"StringBuilder" google that!!!!
-    @Override
-    public String toString() {
-        //add the current player index and deck to the output string.
-        String output = "Current Player Index: " + currentPlayerIndex + "\n";
-        output += "Deck: " + deck + "\n";
-        for (int i = 0; i < NUM_PLAYERS; i++) {
-            output += "Player " + i + " Hand: ";
-            List<Card> hand = playerHands.get(i);
-            for (int j = 0; j < hand.size(); j++) {
-                output += hand.get(j);
-                if (j != hand.size() - 1) {
-                    output += ", ";
-                }
             }
-            output += "\n";
-            output += "Player " + i + " Score: " + playerScores.get(i) + "\n";
         }
-        output += "Game Over: " + gameOver + "\n";
-        return output;
+
+        // Get the current player index
+        public int getCurrentPlayerIndex () {
+            return currentPlayerIndex;
+        }
+
+        // Set the current player index
+        public void setCurrentPlayerIndex ( int currentPlayerIndex){
+            this.currentPlayerIndex = currentPlayerIndex;
+        }
+
+        // Get the deck
+        public List<Card> getDeck () {
+            return deck;
+        }
+
+        // Set the deck
+        public void setDeck (List <Card> deck) {
+            this.deck = deck;
+        }
+
+        // Get the player hands
+        public List<Card> getPlayerHands () {
+            return playerHands;
+        }
+
+        public List<Card> getOpponentHands(){
+        return opponentHands;
+        }
+
+        // Set the player hands
+        public void setPlayerHands (List<Card> playerHands) {
+            this.playerHands = playerHands;
+        }
+
+        public void setOpponentHands(List<Card> opponentHands){
+        this.opponentHands = opponentHands;
+        }
+
+        // Get the player scores
+        public int getPlayerScore (int playerScore){
+            return playerScore;
+        }
+        public int getOpponentScore () {
+            return opponentScore;
+        }
+
+        // Set the player scores
+        public void setPlayerScore (int playerScore){
+            this.playerScore = playerScore;
+        }
+
+        // Check if the game is over
+        public boolean isGameOver() {
+            return gameOver;
+        }
+
+        // Set the game over state
+        public void setGameOver(boolean gameOver){
+            this.gameOver = gameOver;
+        }
+
+        //This toString print the state of the game.
+        //"StringBuilder" google that!!!!
+//        @Override
+//        public String toString () {
+//            //add the current player index and deck to the output string.
+//            String output = "Current Player Index: " + currentPlayerIndex + "\n";
+//            output += "Deck: " + deck + "\n";
+//            for (int i = 0; i < NUM_PLAYERS; i++) {
+//                output += "Player " + i + " Hand: ";
+//                List<Card> hand = playerHands.get(i);
+//                for (int j = 0; j < hand.size(); j++) {
+//                    output += hand.get(j);
+//                    if (j != hand.size() - 1) {
+//                        output += ", ";
+//                    }
+//                }
+//                output += "\n";
+//                output += "Player " + i + " Score: " + playerScore.get(i) + "\n";
+//            }
+//            output += "Game Over: " + gameOver + "\n";
+//            return output;
+//        }
+//    }
     }
-}
+
 
