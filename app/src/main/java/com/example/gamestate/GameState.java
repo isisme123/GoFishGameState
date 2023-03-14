ff27193bd2d7cdd426317e4e1b506fbd158643cd
isisme123 <timogene25@up.edu>
2023-03-14 00:03:10 -0700

    4 new methods & toString

--- app/src/main/java/com/example/gamestate/GameState.java
+++ app/src/main/java/com/example/gamestate/GameState.java
@@ -1,5 +1,7 @@
 package com.example.gamestate;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,7 +15,8 @@ public class GameState {
     public static final int HAND_SIZE = 7;
 
     // Instance variables
-    private int currentPlayerIndex; //player who's turn it is
+    private int playerIndex; //player who's turn it is
+    private int opponentIndex;
     public List<Card> deck;
     public List<Card> playerHands; //Map of the player ID to their hand of cards
     public List<Card> opponentHands;
@@ -24,20 +27,13 @@ public class GameState {
 
     // Constructor
     public GameState() {
-        currentPlayerIndex = 0;
+        playerIndex = 0;
+        opponentIndex = 0;
         deck = createDeck();
-        //deck = new ArrayList<>();
         playerScore = 0;
         opponentScore = 0;
-        //playerHands = NULL;
-//        playerHands = new HashMap<>();
-//        playerHands.put(0, new ArrayList<Card>());
-//        playerHands.put(1, new ArrayList<Card>());
         dealCards(playerHands);
         dealCards(opponentHands);
-//        playerScores = new HashMap<>();
-//        playerScores.put(0, 0);
-//        playerScores.put(1, 0);
         gameOver = false;
     }
 
@@ -52,101 +48,163 @@ public class GameState {
         return deck;
     }
 
-        // Deal the cards to the players
-        public void dealCards (List < Card > j) {
-            for (int i = 0; i < HAND_SIZE; i++) {
-                Random random = new Random();
-                int k = random.nextInt(deck.size());
-                j.add(deck.get(k));
-                deck.remove(k);
+    // Deal the cards to the players
+    public void dealCards (List < Card > j) {
+        for (int i = 0; i < HAND_SIZE; i++) {
+            Random random = new Random();
+            int k = random.nextInt(deck.size());
+            j.add(deck.get(k));
+            deck.remove(k);
 
+        }
+    }
+    // Ask for a card
+    public boolean askForCard(int playerIndex, int rank) {
+        boolean found = false;
+        if (playerIndex == this.playerIndex && rank > 0 && rank <= 13) {
+            for (int i = 0; i < opponentHands.size(); i++) {
+                if (opponentHands.get(i).getRank() == rank) {
+                    playerHands.add(opponentHands.remove(i));
+                    found = true;
+                }
+            }
+            if (found) {
+                if (checkForFour(playerHands, rank)) {
+                    playerScore++;
+                }
+                return true;
             }
         }
-
-        // Get the current player index
-        public int getCurrentPlayerIndex () {
-            return currentPlayerIndex;
-        }
-
-        // Set the current player index
-        public void setCurrentPlayerIndex ( int currentPlayerIndex){
-            this.currentPlayerIndex = currentPlayerIndex;
-        }
-
-        // Get the deck
-        public List<Card> getDeck () {
-            return deck;
-        }
-
-        // Set the deck
-        public void setDeck (List <Card> deck) {
-            this.deck = deck;
-        }
-
-        // Get the player hands
-        public List<Card> getPlayerHands () {
-            return playerHands;
-        }
-
-        public List<Card> getOpponentHands(){
-        return opponentHands;
-        }
-
-        // Set the player hands
-        public void setPlayerHands (List<Card> playerHands) {
-            this.playerHands = playerHands;
-        }
-
-        public void setOpponentHands(List<Card> opponentHands){
-        this.opponentHands = opponentHands;
-        }
-
-        // Get the player scores
-        public int getPlayerScore (int playerScore){
-            return playerScore;
-        }
-        public int getOpponentScore () {
-            return opponentScore;
-        }
-
-        // Set the player scores
-        public void setPlayerScore (int playerScore){
-            this.playerScore = playerScore;
-        }
-
-        // Check if the game is over
-        public boolean isGameOver() {
-            return gameOver;
-        }
-
-        // Set the game over state
-        public void setGameOver(boolean gameOver){
-            this.gameOver = gameOver;
-        }
-
-        //This toString print the state of the game.
-        //"StringBuilder" google that!!!!
-//        @Override
-//        public String toString () {
-//            //add the current player index and deck to the output string.
-//            String output = "Current Player Index: " + currentPlayerIndex + "\n";
-//            output += "Deck: " + deck + "\n";
-//            for (int i = 0; i < NUM_PLAYERS; i++) {
-//                output += "Player " + i + " Hand: ";
-//                List<Card> hand = playerHands.get(i);
-//                for (int j = 0; j < hand.size(); j++) {
-//                    output += hand.get(j);
-//                    if (j != hand.size() - 1) {
-//                        output += ", ";
-//                    }
-//                }
-//                output += "\n";
-//                output += "Player " + i + " Score: " + playerScore.get(i) + "\n";
-//            }
-//            output += "Game Over: " + gameOver + "\n";
-//            return output;
-//        }
-//    }
+        return false;
     }
+
+    //Go fish to determine a legal move
+    public boolean goFish(int playerIndex) {
+        if (playerIndex == this.playerIndex) {
+            Random random = new Random();
+            int k = random.nextInt(deck.size());
+            Card cardDrawn = deck.get(k);
+            playerHands.add(cardDrawn);
+            deck.remove(k);
+            if (checkForFour(playerHands, cardDrawn.getRank())) {
+                playerScore++;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    // Check for four of a kind
+    //helper method
+    private boolean checkForFour(List<Card> hand, int rank) {
+        int count = 0;
+        for (int i = 0; i < hand.size(); i++) {
+            if (hand.get(i).getRank() == rank) {
+                count++;
+            }
+        }
+        return count == 4;
+    }// Draw card from deck
+    public boolean drawCard(int playerIndex) {
+        if (playerIndex == this.playerIndex) {
+            if (deck.size() > 0) {
+                playerHands.add(deck.remove(0));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Get the current player index
+    public int getCurrentPlayerIndex () {
+        return playerIndex;
+    }
+
+    // Set the current player index
+    public void setCurrentPlayerIndex ( int currentPlayerIndex){
+        this.playerIndex = currentPlayerIndex;
+    }
+
+    public int getOpponentIndex () {
+        return opponentIndex;
+    }
+
+    // Set the current player index
+    public void setOpponentIndex ( int opponentIndex){
+        this.opponentIndex = opponentIndex;
+    }
+
+    // Get the deck
+    public List<Card> getDeck () {
+        return deck;
+    }
+
+    // Set the deck
+    public void setDeck (List<Card> deck) {
+        this.deck = deck;
+    }
+
+    // Get the player hands
+    public List<Card> getPlayerHands () {
+        return playerHands;
+    }
+
+    public List<Card> getOpponentHands(){
+        return opponentHands;
+    }
+
+    // Set the player hands
+    public void setPlayerHands (List<Card> playerHands) {
+        this.playerHands = playerHands;
+    }
+
+    public void setOpponentHands(List<Card> opponentHands){
+        this.opponentHands = opponentHands;
+    }
+
+    // Get the player scores
+    public int getPlayerScore (int playerScore){
+        return playerScore;
+    }
+    public int getOpponentScore () {
+        return opponentScore;
+    }
+
+    // Set the player scores
+    public void setPlayerScore (int playerScore){
+        this.playerScore = playerScore;
+    }
+
+    // Check if the game is over
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    // Set the game over state
+    public void setGameOver(boolean gameOver){
+        this.gameOver = gameOver;
+    }
+
+    //This toString print the state of the game.
+    //String concatenation?! THIS MIGHT BE WRONG HEHE, IDK WHAT I'M DOING
+    @Override
+    public String toString() {
+        return "GameState{" +
+                "playerIndex=" + playerIndex +
+                ", opponentIndex=" + opponentIndex +
+                ", deck=" + deck +
+                ", playerHands=" + playerHands +
+                ", opponentHands=" + opponentHands +
+                ", playerScore=" + playerScore +
+                ", opponentScore=" + opponentScore +
+                ", gameOver=" + gameOver +
+                '}';
+    }
+
+    //Printing the current state of the Go Fish game
+    GameState gameState = new GameState();
+    String printState = "The game state is: " + gameState.toString();
+    Log.i("Go Fish", printState);
+}
 
 
