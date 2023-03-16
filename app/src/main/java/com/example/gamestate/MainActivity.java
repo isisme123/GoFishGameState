acfb29e4849418add7ec74bdf6fe23884fd55c72
Alexis Nguyen <alexisnguyen18@icloud.com>
2023-03-16 14:49:34 -0700

    onClick

--- app/src/main/java/com/example/gamestate/MainActivity.java
+++ app/src/main/java/com/example/gamestate/MainActivity.java
@@ -24,6 +24,51 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Perform action here
+
+
+
+
+
+                // Resets editText
+                editText.setText("");
+
+                // First Instance
+                GameState firstInstance = new GameState();
+
+                // Second Instance
+                GameState secondInstance = new GameState(firstInstance);
+
+                // Calling each method
+                firstInstance.askForCard(firstInstance.getCurrentPlayerIndex(), 0);
+                editText.append("Player " + firstInstance.getCurrentPlayerIndex() + "asked for a " + "\n");
+                    // Is there a getter for the card or something? Because I would theoretically use it here
+
+                firstInstance.drawCard(firstInstance.getCurrentPlayerIndex());
+                editText.append("Player " + firstInstance.getCurrentPlayerIndex() + "drew a " + "\n");
+                    // And here too
+
+                firstInstance.goFish(firstInstance.getCurrentPlayerIndex());
+                editText.append("Player " + firstInstance.getCurrentPlayerIndex() +
+                        "drew from the Go Fish pile!\n");
+
+                firstInstance.isGameOver();
+                editText.append("GAME OVER!\n");
+
+                // Third Instance
+                GameState thirdInstance = new GameState();
+
+                // Fourth Instance
+                GameState fourthInstance = new GameState(thirdInstance);
+
+                // Calling toString method on second and fourth instances
+                editText.append(secondInstance.toString()+"\n");
+                editText.append(fourthInstance.toString()+"\n");
+
+
+
+
+
+
                 EditText edit_text = (EditText) findViewById(R.id.edit_text);
                 //Printing the current state of the Go Fish game
                 GameState gameState = new GameState();
